cb1652e5bccc42db64a614b90ec47d8d9d122989
Alex Larson <aplarson@fortlewis.edu>
2026-03-08 14:59:04 -0600

    added reset ball and some momentum transfer

--- GodotServer/game_root.gd
+++ GodotServer/game_root.gd
@@ -1,23 +1,53 @@
 extends Node3D
 
-@export var ball_start_position: Vector3 = Vector3(0, 2, 0)
+var ball_start_position: Vector3 = Vector3(0, 1, -1.5)
 @onready var ball: RigidBody3D = $Ball
+@onready var paddle = $Paddle1
+@onready var reset_box = $ResetBox
 
-var normal_gravity_scale: float = 1.0
-var gravity_enabled: bool = false
+
+var normal_gravity_scale := 1.0
+var gravity_enabled := false
+var can_reset := true
+
 
 func _ready():
 	normal_gravity_scale = ball.gravity_scale
+	ball.body_entered.connect(_on_ball_body_entered)
 	reset_ball()
+	
+	
+	
+func _process(delta):
+	if can_reset and paddle.global_position.distance_to(reset_box.global_position) < 0.25:
+		print("Reset Ball")
+		reset_ball()
+		can_reset = false
+		await get_tree().create_timer(0.5).timeout
+		can_reset = true
 
-func _physics_process(delta):
-	if not gravity_enabled and ball.get_contact_count() > 0:
-		ball.gravity_scale = normal_gravity_scale
-		gravity_enabled = true
-
-func reset_ball():
+func reset_ball(): # Sets the ball to have gravity off and in a preset position on startup
+	ball.freeze = true
 	ball.global_position = ball_start_position
 	ball.linear_velocity = Vector3.ZERO
 	ball.angular_velocity = Vector3.ZERO
 	ball.gravity_scale = 0.0
 	gravity_enabled = false
+	ball.freeze = false
+
+func _on_ball_body_entered(body):
+	if not gravity_enabled:
+		ball.gravity_scale = normal_gravity_scale
+		gravity_enabled = true
+		print("gravity turned on after collision with ", body.name)
+
+	if body.name == "Paddle1":
+		print("Paddle velocity: ", paddle.tracked_velocity)
+		ball.linear_velocity += paddle.tracked_velocity * 0.8
+		print("Ball velocity after hit: ", ball.linear_velocity)
+		
+		
+
+		
+
+		
---
+++ GodotServer/paddle.gd
@@ -0,0 +1,18 @@
+extends StaticBody3D
+
+
+var previous_position: Vector3
+var tracked_velocity: Vector3 = Vector3.ZERO
+
+func _ready():
+	previous_position = global_position
+
+func _process(delta):
+	calculate_velocity(delta)
+
+
+func calculate_velocity(delta):
+	if delta > 0:
+		tracked_velocity = (global_position - previous_position) / delta
+		#print("Tracked velocity: ", tracked_velocity)
+	previous_position = global_position
